4c3e0a6b49dcd2ff450a272ee0758fa8d25147e2
inest-us <hieung@live.com>
2017-07-10 03:36:28 -0700

    9th check-in

--- blog/admin/add-post.php
+++ blog/admin/add-post.php
@@ -1,79 +1,95 @@
 <?php
 //include config
 require_once('../includes/config.php');
-require_once('menu.php');
 
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
-
-//if form has been submitted process it
-if(isset($_POST['submit'])){
-    $_POST = array_map('stripslashes', $_POST);
-
-    //collect form data
-    extract($_POST);
-
-    //very basic validation
-    if($postTitle ==''){
-        $error[] = 'Please enter the title.';
-    }
-
-    if($postDesc ==''){
-        $error[] = 'Please enter the description.';
-    }
-
-    if($postContent ==''){
-        $error[] = 'Please enter the content.';
-    }
-
-    if(!isset($error)) { 
-        try {
-            //insert into database
-            $stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postContent,postDate) VALUES (:postTitle, :postDesc, :postContent, :postDate)');
-            $stmt->execute(array(
-                ':postTitle' => $postTitle,
-                ':postDesc' => $postDesc,
-                ':postContent' => $postContent,
-                ':postDate' => date('Y-m-d H:i:s')
-            ));
-            //redirect to index page
-            header('Location: index.php?action=added');
-            exit;
-        } catch(PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    if(isset($error)) {
-        foreach($error as $error){
-            echo '<p class="error">'.$error.'</p>';
-        }
-    }
-}
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin - Add Post</title>
+    <link rel="stylesheet" href="../style/normalize.css">
+    <link rel="stylesheet" href="../style/main.css">
+    <script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+    <script>
+            tinymce.init({
+                selector: "textarea",
+                plugins: [
+                    "advlist autolink lists link image charmap print preview anchor",
+                    "searchreplace visualblocks code fullscreen",
+                    "insertdatetime media table contextmenu paste"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+            });
+    </script>
+</head>
+<body>
 
-<form action='' method='post'>
-    <p><label>Title</label><br />
-    <input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
+    <div id="wrapper">
+        <?php include('menu.php');?>
+        <p><a href="./">Blog Admin Index</a></p>
+        <h2>Add Post</h2>
+        <?php
+        //if form has been submitted process it
+        if(isset($_POST['submit'])){
+            $_POST = array_map('stripslashes', $_POST);
 
-    <p><label>Description</label><br />
-    <textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
+            //collect form data
+            extract($_POST);
 
-    <p><label>Content</label><br />
-    <textarea name='postContent' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postContent'];}?></textarea></p>
+            //very basic validation
+            if($postTitle ==''){
+                $error[] = 'Please enter the title.';
+            }
 
-    <p><input type='submit' name='submit' value='Submit'></p>
-</form>
+            if($postDesc ==''){
+                $error[] = 'Please enter the description.';
+            }
 
-<script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
-<script>
-        tinymce.init({
-            selector: "textarea",
-            plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-        });
-</script>
+            if($postContent ==''){
+                $error[] = 'Please enter the content.';
+            }
+
+            if(!isset($error)) { 
+                try {
+                    //insert into database
+                    $stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postContent,postDate) VALUES (:postTitle, :postDesc, :postContent, :postDate)');
+                    $stmt->execute(array(
+                        ':postTitle' => $postTitle,
+                        ':postDesc' => $postDesc,
+                        ':postContent' => $postContent,
+                        ':postDate' => date('Y-m-d H:i:s')
+                    ));
+                    //redirect to index page
+                    header('Location: index.php?action=added');
+                    exit;
+                } catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
+            }
+
+            if(isset($error)) {
+                foreach($error as $error){
+                    echo '<p class="error">'.$error.'</p>';
+                }
+            }
+        }
+        ?>
+
+        <form action='' method='post'>
+            <p><label>Title</label><br />
+            <input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
+
+            <p><label>Description</label><br />
+            <textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
+
+            <p><label>Content</label><br />
+            <textarea name='postContent' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postContent'];}?></textarea></p>
+
+            <p><input type='submit' name='submit' value='Submit'></p>
+        </form>
+    </div>
+</body>
+</html>
--- blog/admin/edit-post.php
+++ blog/admin/edit-post.php
@@ -1,94 +1,109 @@
 <?php
 //include config
 require_once('../includes/config.php');
-require_once('menu.php');
 
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
-
-try {
-    $stmt = $db->prepare('SELECT postID, postTitle, postDesc, postContent FROM blog_posts WHERE postID = :postID');
-    $stmt->execute(array(':postID' => $_GET['id']));
-    $row = $stmt->fetch(); 
-} catch(PDOException $e) {
-    echo $e->getMessage();
-}
-
-//if form has been submitted process it
-if(isset($_POST['submit'])) {
-    $_POST = array_map('stripslashes', $_POST);
-
-    //collect form data
-    extract($_POST);
-
-    //very basic validation
-    if($postID ==''){
-        $error[] = 'This post is missing a valid id!.';
-    }
-
-    if($postTitle ==''){
-        $error[] = 'Please enter the title.';
-    }
-
-    if($postDesc ==''){
-        $error[] = 'Please enter the description.';
-    }
-
-    if($postContent ==''){
-        $error[] = 'Please enter the content.';
-    }
-
-    if(!isset($error)) { 
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin - Edit Post</title>
+    <link rel="stylesheet" href="../style/normalize.css">
+    <link rel="stylesheet" href="../style/main.css">
+    <script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+    <script>
+            tinymce.init({
+                selector: "textarea",
+                plugins: [
+                    "advlist autolink lists link image charmap print preview anchor",
+                    "searchreplace visualblocks code fullscreen",
+                    "insertdatetime media table contextmenu paste"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+            });
+    </script>
+</head>
+<body>
+    <div id="wrapper">
+        <?php include('menu.php');?>
+        <p><a href="./">Blog Admin Index</a></p>
+        <h2>Edit Post</h2>
+        <?php
         try {
-            //insert into database
-            $stmt = $db->prepare('UPDATE blog_posts SET postTitle = :postTitle, postDesc = :postDesc, postContent = :postContent WHERE postID = :postID') ;
-            $stmt->execute(array(
-                ':postTitle' => $postTitle,
-                ':postDesc' => $postDesc,
-                ':postContent' => $postContent,
-                ':postID' => $postID
-            ));
-
-            //redirect to index page
-            header('Location: index.php?action=updated');
-            exit;
+            $stmt = $db->prepare('SELECT postID, postTitle, postDesc, postContent FROM blog_posts WHERE postID = :postID');
+            $stmt->execute(array(':postID' => $_GET['id']));
+            $row = $stmt->fetch(); 
         } catch(PDOException $e) {
             echo $e->getMessage();
         }
-    }
 
-    if(isset($error)) {
-        foreach($error as $error){
-            echo '<p class="error">'.$error.'</p>';
+        //if form has been submitted process it
+        if(isset($_POST['submit'])) {
+            $_POST = array_map('stripslashes', $_POST);
+
+            //collect form data
+            extract($_POST);
+
+            //very basic validation
+            if($postID ==''){
+                $error[] = 'This post is missing a valid id!.';
+            }
+
+            if($postTitle ==''){
+                $error[] = 'Please enter the title.';
+            }
+
+            if($postDesc ==''){
+                $error[] = 'Please enter the description.';
+            }
+
+            if($postContent ==''){
+                $error[] = 'Please enter the content.';
+            }
+
+            if(!isset($error)) { 
+                try {
+                    //insert into database
+                    $stmt = $db->prepare('UPDATE blog_posts SET postTitle = :postTitle, postDesc = :postDesc, postContent = :postContent WHERE postID = :postID') ;
+                    $stmt->execute(array(
+                        ':postTitle' => $postTitle,
+                        ':postDesc' => $postDesc,
+                        ':postContent' => $postContent,
+                        ':postID' => $postID
+                    ));
+
+                    //redirect to index page
+                    header('Location: index.php?action=updated');
+                    exit;
+                } catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
+            }
+
+            if(isset($error)) {
+                foreach($error as $error){
+                    echo '<p class="error">'.$error.'</p>';
+                }
+            }
         }
-    }
-}
-?>
+        ?>
 
-<form action='' method='post'>
-    <input type='hidden' name='postID' value='<?php echo $row['postID'];?>'>
+        <form action='' method='post'>
+            <input type='hidden' name='postID' value='<?php echo $row['postID'];?>'>
 
-    <p><label>Title</label><br />
-    <input type='text' name='postTitle' value='<?php echo $row['postTitle'];?>'></p>
+            <p><label>Title</label><br />
+            <input type='text' name='postTitle' value='<?php echo $row['postTitle'];?>'></p>
 
-    <p><label>Description</label><br />
-    <textarea name='postDesc' cols='60' rows='10'><?php echo $row['postDesc'];?></textarea></p>
+            <p><label>Description</label><br />
+            <textarea name='postDesc' cols='60' rows='10'><?php echo $row['postDesc'];?></textarea></p>
 
-    <p><label>Content</label><br />
-    <textarea name='postContent' cols='60' rows='10'><?php echo $row['postContent'];?></textarea></p>
+            <p><label>Content</label><br />
+            <textarea name='postContent' cols='60' rows='10'><?php echo $row['postContent'];?></textarea></p>
 
-    <p><input type='submit' name='submit' value='Update'></p>
-</form>
-
-<script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
-<script>
-        tinymce.init({
-            selector: "textarea",
-            plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-        });
-</script>
+            <p><input type='submit' name='submit' value='Update'></p>
+        </form>
+    </div>
+</body>
+</html>
