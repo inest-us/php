52678c4c182dc677091e9cc3176742e3c7dda0e4
inest-us <hieung@live.com>
2019-12-28 16:02:46 -0800

    update blog home page

--- blog/index.php
+++ blog/index.php
@@ -15,15 +15,18 @@
         <?php
             try {
                 $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                echo '<ol>';
                 while($row = $stmt->fetch()){
-                    
-                    echo '<div>';
-                        echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-                        echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-                        echo '<p>'.$row['postDesc'].'</p>';                
-                        echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';                
-                    echo '</div>';
+                    echo '<li>';
+                        echo '<div>';
+                            echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+                            echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+                            echo '<p>'.$row['postDesc'].'</p>';                
+                            echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';                
+                        echo '</div>';
+                    echo '</li>';
                 }
+                echo '</ol>';
             } catch(PDOException $e) {
                 echo $e->getMessage();
             }
