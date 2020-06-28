c6e77ebbf3706e252bab6c444bf30fd5da7386ee
inest-us <hieung@live.com>
2020-06-28 11:39:00 -0700

    Revert "check-in 2020-06-28"
    This reverts commit 6e03c8fc0f46776e232e83e1c6ddbe644a5b0219.

--- dan/db/CreateUsersTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`username` VARCHAR( 255 ) NOT NULL ,
-`email` VARCHAR( 255 ) NOT NULL ,
-`password` VARCHAR( 8 ) NOT NULL ,
-`status` ENUM( 'active', 'inactive' ) NOT NULL
-) ENGINE = MYISAM ;
