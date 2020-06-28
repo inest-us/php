516bc10989d22b65df0dc2de832d8f467a8b3eaa
inest-us <hieung@live.com>
2020-06-27 21:32:30 -0700

    sample feedback form

--- php101/c04/processfeedback.php
+++ php101/c04/processfeedback.php
@@ -1,10 +1,29 @@
 <?php
     //create short variable names
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $feedback=$_POST['feedback'];
+    $name = addslashes(trim($_POST['name']));
+    $email = addslashes(trim($_POST['email']));
+    $feedback = addslashes(trim($_POST['feedback']));
+    $email_array = explode('@', $email);
+
+    if (!eregi('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$', $email)) {
+        echo "<p>That is not a valid email address.</p>".
+                "<p>Please return to the previous page and try again.</p>";
+        exit;
+    }
+    
     //set up some static information
-    $toaddress = "feedback@example.com";
+    if (strtolower($email_array[1]) == "bigcustomer.com") {
+        $toaddress = "bob@example.com";
+    } else {
+        $toaddress = "feedback@example.com";
+        if (strstr($feedback, 'shop')) {
+            $toaddress = "retail@example.com";
+        } else if (strstr($feedback, 'delivery')) {
+            $toaddress = "fulfillment@example.com";
+        } else if (strstr($feedback, 'bill')) {
+            $toaddress = "account@example.com";
+        }
+    }
     $subject = "Feedback from web site";
     $mailcontent = "Customer name: ".$name."\n".
                 "Customer email: ".$email."\n".
@@ -19,6 +38,11 @@
 </head>
 <body>
     <h1>Feedback submitted</h1>
-    <p>Your feedback has been sent.</p>
+    <p>Your feedback (shown below) has been sent.</p>
+    <p>
+        <?php
+            echo nl2br($mailcontent);
+        ?>
+    </p>
 </body>
 </html>
