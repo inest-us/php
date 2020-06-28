b1b8c3166a6906070ad711c432cb230f86d4242c
inest-us <hieung@live.com>
2020-06-27 23:23:36 -0700

    Classes and Objects

---
+++ php101/c04/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TLA Consulting Pty Ltd</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!-- page header -->
+        <table width="100%" cellpadding="12" cellspacing="0" border="0">
+            <tr bgcolor="black">
+                <td align="left">
+                    <img src="logo.gif" alt="TLA logo" height="70" width="70" />
+                </td>
+                <td>
+                    <h1>TLA Consulting</h1>
+                </td>
+                <td align="right">
+                    <img src="logo.gif" alt="TLA logo" height="70" width="70" />
+                </td>
+            </tr>
+        </table>
+        <!-- menu -->
+        <table width="100%" bgcolor="white" cellpadding="4" cellspacing="4">
+            <tr>
+                <td width="25%">
+                    <img src="s-logo.gif" alt=" " height="20" width="20" />
+                    <span class="menu">Home</span>
+                </td>
+                <td width="25%">
+                    <img src="s-logo.gif" alt=" " height="20" width="20" />
+                    <span class="menu">Contact</span>
+                </td>
+                <td width="25%">
+                    <img src="s-logo.gif" alt=" " height="20" width="20">
+                    <span class="menu">Services</span></td>
+                </td>
+                <td width="25%">
+                    <img src="s-logo.gif" alt=" " height="20" width="20">
+                    <span class="menu">Site Map</span>
+                </td>
+            </tr>
+        </table>
