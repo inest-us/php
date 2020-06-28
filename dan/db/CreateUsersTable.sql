6e03c8fc0f46776e232e83e1c6ddbe644a5b0219
inest-us <hieung@live.com>
2020-06-28 11:37:28 -0700

    check-in 2020-06-28

---
+++ dan/db/CreateUsersTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`username` VARCHAR( 255 ) NOT NULL ,
+`email` VARCHAR( 255 ) NOT NULL ,
+`password` VARCHAR( 8 ) NOT NULL ,
+`status` ENUM( 'active', 'inactive' ) NOT NULL
+) ENGINE = MYISAM ;
