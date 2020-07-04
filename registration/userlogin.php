ad232aa7a0686efae81a2b8e0d9c31cbdbfefadd
inest-us <hieung@live.com>
2020-07-04 16:15:19 -0700

    add user login page

--- registration/userlogin.php
+++ registration/userlogin.php
@@ -6,30 +6,22 @@
 </head>
 <body>
     <div class="header">
-        <h2>Register</h2>
+        <h2>Login</h2>
     </div>
-    <form method="post" action="register.php">
+    <form method="post" action="userlogin.php">
         <div class="input-group">
             <label>Username</label>
             <input type="text" name="username">
-        </div>
-        <div class="input-group">
-            <label>Email</label>
-            <input type="text" name="email">
         </div>
         <div class="input-group">
             <label>Password</label>
             <input type="password" name="password_1">
         </div>
         <div class="input-group">
-            <label>Confirm Password</label>
-            <input type="password" name="password_2">
-        </div>
-        <div class="input-group">
-            <button type="submit" name="register" class="btn">Register</button>
+            <button type="submit" name="login" class="btn">Login</button>
         </div>
         <p>
-            Already a member? <a href="userlogin.php">Sign in</a>
+            Not yet a member? <a href="register.php">Sign up</a>
         </p>
     </form>
 </body>
