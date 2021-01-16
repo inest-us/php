a9c884e3fc85abd23e9b29496e1d98ab08a53f3e
inest-us <hieung@live.com>
2021-01-15 17:59:34 -0800

    checck-in 2021-01-15

--- PHP5/Ch03/hidden2.php
+++ PHP5/Ch03/hidden2.php
@@ -1,13 +1,15 @@
 <html>
-<head><title></title></head>
+<head>
+    <title>hidden</title>
+</head>
 <body>
 <?php
-echo "The three options were:<br>";
-echo "$_GET[Hidden1]<br>";
-echo "$_GET[Hidden2]<br>";
-echo "$_GET[Hidden3]<br>";
-echo "<br>You selected:<br>";
-echo "$_GET[ListBox]";
+    echo "The three options were:<br />";
+    echo "$_GET[Hidden1]<br />";
+    echo "$_GET[Hidden2]<br />";
+    echo "$_GET[Hidden3]<br />";
+    echo "<br>You selected:<br />";
+    echo "$_GET[ListBox]";
 ?>
 </body>
 </html>
--- PHP5/Ch03/sessions.php
+++ PHP5/Ch03/sessions.php
@@ -1,29 +1,28 @@
 <?php
-
-session_register('view1count');
-session_register('view2count');
-session_register('view3count');
-session_register('view4count');
+   $_SESSION['view1count'] = "0";
+   $_SESSION['view2count'] = "0";
+   $_SESSION['view3count'] = "0";
+   $_SESSION['view4count'] = "0";
 ?>
 <?php
-
-//The rest of the script illustrates how to make hyperlinks that hand PHP what it needs to access your session data - namely, SID.
-echo "<html><head><title>Web Page Hit Counter</title></head><body>";
-if (isset($_GET['whichpage'])) {
-   echo "<b>You are currently on page $_GET[whichpage].</b><br><br>\n";
-  $_SESSION["view".$_GET['whichpage']."count"]++;
-}
-
-for ($i = 1; $i <= 4; $i++) {
-   if (isset($_GET['whichpage']) == $i) {
-      echo "<b><a href=\"sessions.php?".session_id()."&whichpage=$i\">Page $i</a></b>";
-   } else {
-      echo "<a href=\"sessions.php?".session_id()."&whichpage=$i\">Page $i</a>";
+   // The rest of the script illustrates how to make hyperlinks that hand PHP what it needs to access your session data - namely, SID.
+   echo "<html><head><title>Web Page Hit Counter</title></head><body>";
+   if (isset($_GET['whichpage'])) {
+      echo "<b>You are currently on page $_GET[whichpage].</b><br><br>\n";
+      $_SESSION["view" . $_GET['whichpage'] . "count"]++;
    }
-if (!isset($_SESSION["view".$i."count"])) $_SESSION["view".$i."count"] = 0;
-   echo ", which you have chosen ".$_SESSION["view".$i."count"]." times.<BR>\n";
-}
-echo "\n\n<br><br>\n\n";
-echo "</body></html>";
 
+   for ($i = 1; $i <= 4; $i++) {
+      if (isset($_GET['whichpage']) == $i) {
+         echo "<b><a href=\"sessions.php?".session_id()."&whichpage=$i\">Page $i</a></b>";
+      } else {
+         echo "<a href=\"sessions.php?".session_id()."&whichpage=$i\">Page $i</a>";
+      }
+      if (!isset($_SESSION["view".$i."count"])) {
+         $_SESSION["view".$i."count"] = 0;
+      }
+      echo ", which you have chosen ".$_SESSION["view".$i."count"]." times.<BR>\n";
+   }
+   echo "\n\n<br><br>\n\n";
+   echo "</body></html>";
 ?>
